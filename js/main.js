69c063386765c3d52d3cccab420c2274b548921a
Wesli-S <wesli.smart1017@gmail.com>
2023-11-09 11:18:09 -0800

    Added version that does not use HTML elements

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-window.onload = function () {
-    const calculate = document.getElementById("convert-decimal");
-    calculate.onclick = DecimalToBinary;
-};
-let decimalTextBox = document.querySelector("#decimal");
-function DecimalToBinary() {
-    const decimal = parseInt(decimalTextBox.value);
-    const binary = decimal.toString(2);
-    alert(`The binary representation of ${decimal} is ${binary}.`);
-}
-let isValidData = true;
-let decimal = parseInt(decimalTextBox.value);
-if (isNaN(decimal) || decimal < 0) {
-    isValidData = false;
-    decimalTextBox.nextElementSibling.textContent = "Please enter a valid number";
+function DecimalToBinary(decimal) {
+    let binary = "";
+    while (decimal > 0) {
+        binary = (decimal % 2) + binary;
+        decimal = Math.floor(decimal / 2);
+    }
+    return binary;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,7 @@
 /*Create a function that takes a decimal number as a 
 parameter and returns the binary equivalent as a string */
-window.onload = function(){
+
+/*window.onload = function(){
     //set up button click
     const calculate = document.getElementById("convert-decimal") as HTMLButtonElement;
     calculate.onclick = DecimalToBinary;
@@ -16,6 +17,7 @@ function DecimalToBinary() {
     alert(`The binary representation of ${decimal} is ${binary}.`);
 }
 
+
 //validate input
 let isValidData:boolean = true;
 
@@ -23,5 +25,14 @@ let decimal = parseInt(decimalTextBox.value);
 if(isNaN(decimal) || decimal < 0) {
     isValidData = false;
     decimalTextBox.nextElementSibling.textContent = "Please enter a valid number";
-}
+}*/
 
+ //Another version (DOES NOT USE HTML ELEMENTS)
+function DecimalToBinary(decimal: number): string {
+    let binary = "";
+    while (decimal > 0) {
+        binary = (decimal % 2) + binary;
+        decimal = Math.floor(decimal / 2);
+    }
+    return binary
+}
